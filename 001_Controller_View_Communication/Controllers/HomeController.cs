ec16f1412639455c0de48478bdac5495414a253e
Furkan Coskun <furkancoskunn96@gmail.com>
2022-04-30 19:44:34 +0300

    Controller'dan View'a Veri Yollama
    1. Model
    2. ViewBag
    3. ViewData
    4. TempData

---
+++ 001_Controller_View_Communication/Controllers/HomeController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace _001_Controller.View.Communication.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            TempData["Data"] = "Test-TempData";
+
+            ViewData["Data"] = "Test-ViewData";
+
+            ViewBag.Data = "Test-ViewBag";
+
+            return RedirectToAction(
+                    actionName: "Index", 
+                    controllerName: "Redirect"
+                );
+        }
+    }
+}
---
+++ 001_Controller_View_Communication/Controllers/RedirectController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace _001_Controller.View.Communication.Controllers
+{
+    public class RedirectController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}

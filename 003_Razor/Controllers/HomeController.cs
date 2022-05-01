73a69178b95d2438a860318bf82199476bbb8c03
Furkan Coskun <furkancoskunn96@gmail.com>
2022-05-01 14:02:28 +0300

    Razor Teknolojisi

---
+++ 003_Razor/Controllers/HomeController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace _003_Razor.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            ViewBag.Operation = Operation.Division;
+
+            return View();
+        }
+    }
+
+    public enum Operation
+    {
+        Addition,
+        Subtraction,
+        Multiplication,
+        Division
+    }
+}
